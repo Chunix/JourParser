65404d1882bd56d826f65022f7db9a7e28822b44
chrhong <christian.hong@nokia-sbell.com>
2020-04-13 18:04:11 +0800

    Refactor and release v1.0

--- JourParser.py
+++ JourParser.py
@@ -1,9 +1,15 @@
-import sys
 import os
+import sys
+import json
+import threading
+
+import winreg
 import paramiko
 from paramiko import SSHClient
-import json
-import winreg
+from tkinter import *
+from tkinter import ttk
+from tkinter.filedialog import askdirectory
+from tkinter.filedialog import askopenfilename
 
 CONFIG_FILE = "configure.json"
 
@@ -14,100 +20,6 @@ def hang_up_to_watch_errors():
         pass
 
     sys.exit()
-
-def journal_paser(source_file, config_dict):
-    step_keyword = "Read Configuration"
-    ret_bool = True
-
-    try:
-        print("Journal parser start!")
-
-        user_name = config_dict["user_name"]
-        user_pwd = config_dict["user_pwd"]
-        host_ip = config_dict["host_ip"]
-        remote_path = config_dict["remote_path"]
-
-        local_file = source_file.replace('\\', '/').replace('\"', '')
-        local_path = local_file[:local_file.rfind('/')+1]
-        local_name = local_file[local_file.rfind('/')+1:]
-        remote_name = local_name[:local_name.rfind('.')] + '.txt'
-        remote_file = remote_path + remote_name
-
-        print("===========================")
-        print("Local file, path and name:")
-        print(local_file)
-        print(local_path)
-        print(local_name)
-        print("===========================")
-        print("Remote file, path and name:")
-        print(remote_file)
-        print(remote_path)
-        print(remote_name)
-        print("===========================")
-
-        step_keyword = "SSH connection"
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(host_ip, 22, user_name, user_pwd)
-        print("SSH successfully")
-
-        step_keyword = "SFTP connection"
-        tport = paramiko.Transport((host_ip, 22))
-        tport.connect(username=user_name, password=user_pwd)
-        sftp = paramiko.SFTPClient.from_transport(tport)
-        print("SFTP successfully")
-
-        step_keyword = "remove old file"
-        stdin, stdout, stderr = ssh.exec_command("rm -f %s/*" %remote_path)
-        print(stderr.readlines())
-
-        step_keyword = "upload file"
-        sftp.put(local_file, remote_path + local_name)
-        print('Upload file successfully')
-        print('------')
-
-        step_keyword = "paser journal"
-        stdin, stdout, stderr = ssh.exec_command("journalctl -D " + remote_path + " > " + remote_file)
-        print(stderr.readlines())
-
-        step_keyword = "download file"
-        sftp.get(remote_file, local_path + remote_name)
-        print('Download file successfully')
-        print('------')
-
-        ssh.close()
-        tport.close()
-
-        ret_bool = True
-
-    except Exception:
-        print("[Error] " + step_keyword)
-        ret_bool = False
-
-    print("Journal parser end!")
-    return ret_bool
-
-def regist_contextmenu(workpath):
-    workpath = workpath.replace('/', '\\')
-    print('---')
-    print(workpath)
-
-    target_icon = workpath + "\\JourParser.exe"
-    target_cmd = workpath + "\\JourParser.exe \"%1\""
-
-    try:
-        #Not check if it is already exist, since we can overwrite it
-        subkey = winreg.OpenKey(winreg.HKEY_CLASSES_ROOT,"*\\shell")
-        targetkey = winreg.CreateKey(subkey,"JourParser")
-        winreg.SetValueEx(targetkey, "Icon", 0,  winreg.REG_SZ, target_icon)
-        winreg.SetValue(targetkey, "command",  winreg.REG_SZ, target_cmd)
-        winreg.CloseKey(subkey)
-    except OSError:
-        print("[Error] Contextmenu register failed!")
-        hang_up_to_watch_errors()
-        return False
-    else:
-        return True
 
 def config_load():
     # determine if application is a script file or frozen exe
@@ -146,30 +58,186 @@ def config_load():
 
     return dict_str
 
-def main():
+def journal_paser(source_file, progress_bar=None):
+    step_keyword = "Read Configuration"
+    ret_bool = True
+    print(source_file)
+    try:
+        print("Journal parser start!")
 
+        step_keyword = "Configuration load!"
+        config_dict = config_load()
+
+        user_name = config_dict["user_name"]
+        user_pwd = config_dict["user_pwd"]
+        host_ip = config_dict["host_ip"]
+        remote_path = config_dict["remote_path"]
+
+        local_file = source_file.replace('\\', '/').replace('\"', '')
+        local_path = local_file[:local_file.rfind('/')+1]
+        local_name = local_file[local_file.rfind('/')+1:]
+        remote_name = local_name[:local_name.rfind('.')] + '.txt'
+        remote_file = remote_path + remote_name
+
+        print("===========================")
+        print("Local file, path and name:")
+        print(local_file)
+        print(local_path)
+        print(local_name)
+        print("===========================")
+        print("Remote file, path and name:")
+        print(remote_file)
+        print(remote_path)
+        print(remote_name)
+        print("===========================")
+
+        step_keyword = "SSH connection"
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(host_ip, 22, user_name, user_pwd)
+        print("SSH successfully")
+
+        if progress_bar:
+            progress_bar.step(10)
+
+        step_keyword = "SFTP connection"
+        tport = paramiko.Transport((host_ip, 22))
+        tport.connect(username=user_name, password=user_pwd)
+        sftp = paramiko.SFTPClient.from_transport(tport)
+        print("SFTP successfully")
+
+        if progress_bar:
+            progress_bar.step(10)
+
+        step_keyword = "remove old file"
+        stdin, stdout, stderr = ssh.exec_command("rm -f %s/*" %remote_path)
+        print(stderr.readlines())
+
+        if progress_bar:
+            progress_bar.step(10)
+
+        step_keyword = "upload file"
+        sftp.put(local_file, remote_path + local_name)
+        print('Upload file successfully')
+        print('------')
+
+        if progress_bar:
+            progress_bar.step(10)
+
+        step_keyword = "paser journal"
+        stdin, stdout, stderr = ssh.exec_command("journalctl -D " + remote_path + " > " + remote_file)
+        print(stderr.readlines())
+
+        if progress_bar:
+            progress_bar.step(10)
+
+        step_keyword = "download file"
+        sftp.get(remote_file, local_path + remote_name)
+        print('Download file successfully')
+        print('------')
+
+        if progress_bar:
+            progress_bar.step(20)
+
+        ssh.close()
+        tport.close()
+
+        if progress_bar:
+            progress_bar.step(30)
+
+        ret_bool = True
+
+    except Exception:
+        print("[Error] " + step_keyword)
+        ret_bool = False
+
+    print("Journal parser end!")
+    return ret_bool
+
+def regist_contextmenu(workpath):
+    workpath = workpath.replace('/', '\\')
+    print('---')
+    print(workpath)
+
+    target_icon = workpath + "\\JourParser.exe"
+    target_cmd = workpath + "\\JourParser.exe \"%1\""
+
+    try:
+        #Not check if it is already exist, since we can overwrite it
+        subkey = winreg.OpenKey(winreg.HKEY_CLASSES_ROOT,"*\\shell")
+        targetkey = winreg.CreateKey(subkey,"JourParser")
+        winreg.SetValueEx(targetkey, "Icon", 0,  winreg.REG_SZ, target_icon)
+        winreg.SetValue(targetkey, "command",  winreg.REG_SZ, target_cmd)
+        winreg.CloseKey(subkey)
+    except OSError:
+        print("[Error] Contextmenu register failed!")
+        hang_up_to_watch_errors()
+        return False
+    else:
+        return True
+
+# xWindows
+def start_journal_thread(progress_bar, source_file):
+    threading.Thread(target=journal_paser, args={source_file.get(), progress_bar}).start()
+    return
+
+def browse(source_file):
+    file_path = askopenfilename()
+    source_file.delete(0, END)
+    source_file.insert(index=0, string=file_path)
+    print("browse button is clicked, file path:" + file_path)
+
+def xWindows():
+    root = Tk()
+    root.title("Journal Paser 1.0")
+    root.geometry('400x100')
+    root.resizable(False, False)
+    file_location = StringVar()
+
+    source_file = Entry(root, text = 'Source file:', textvariable=file_location, borderwidth=3, width = 50)
+    source_file.place(x=10, y=20, anchor=NW)
+
+    button_browse = Button(root, fg="green", text='Browse', width=8, command=lambda: browse(source_file))
+    button_browse.place(x=320, y=17, anchor=NW)
+
+    progress_bar = ttk.Progressbar(root, orient="horizontal", length=300, mode="determinate", maximum=101)
+    progress_bar.place(x=10, y=60,anchor=NW)
+    progress_bar.step(0)
+
+    button_browse = Button(root, fg="green", text="Let's go", width=8, command=lambda: start_journal_thread(progress_bar,source_file))
+    button_browse.place(x=320, y=57, anchor=NW)
+
+    root.mainloop()
+
+def main():
     config_dict = config_load()
     if not config_dict:
         hang_up_to_watch_errors()
         return
 
     if len(sys.argv) < 2:
-        print("[Error] argv error! Use demo file in debug mode!")
-        #debug file
-        source_file = "D:/userdata/chrhong/Desktop/hong guo/system.journal"
+        print("[Info] no argv! Please right click the target journal file to choose JourParser!")
+        hang_up_to_watch_errors()
+        """
+        Note:
+        gui mode is not convinient, disabled!
+        there is also issues while using this mode:
+        1. outputs is not printed in window while problem detected.
+        2. sometimes journal_paser stop at configure load, which may related with
+           sourcefile configuration.
+        """
+        # xWindows()
     else:
         source_file = ' '.join(sys.argv[1:]).strip("\'\"")
 
-    if not source_file:
-        print("[Error] Null path")
-        hang_up_to_watch_errors()
-        return
+        if not source_file:
+            print("[Error] Null path")
+            hang_up_to_watch_errors()
+            return
 
-    if not journal_paser(source_file, config_dict):
-        hang_up_to_watch_errors()
-        return
-
-    return
+        if not journal_paser(source_file):
+            hang_up_to_watch_errors()
+            return
 
 if __name__ == "__main__":
     main()
--- README.md
+++ README.md
@@ -1,24 +1,26 @@
 # This is a easy journal log parser tool
-#### Usage Steps:
-1. Config the `configure.json` before your first using
+
+The project try to make journal log parsing on windows easily. The basic logic is to transfer the journal log to a remote linux server to convert and pull the result back. So the tool requires **a remote/local linux env which you have access rights**.
+
+#### Package
+`pyinstaller.exe -i JourParser.ico -F JourParser.py`
+
+#### Protable Installation
+1. Download latest [JourParser.zip](). (Access Code: )
+2. Config the `configure.json` before your first using
     * `host_ip`: your remote linux server ip
     * `user_pwd` and `user_name`: your remote linux server ip's username and password
     * `remote_path`: give a your journal folder on your remote linux server. eg: mine is `/root/usr/journal/chrhong/`
-    * `context_menu`: must be `"disable"` before your first using 
-2. Run `JourParser.exe` as administrator first time.
-3. Then you can find a `"JourParser"` item in your contextmenu, click it and you can get a result in the same folder.
+    * `context_menu`: must be `"disable"` before your first using
+3. Run `JourParser.exe` as administrator first time.
+4. Then you can find a `"JourParser"` item in your contextmenu, click it and you can get a result in the same folder.
 
 ![](demo.png)
 
-4. Enjoy it.
+5. Enjoy it.
 
 #### **Note and Warnings !**
 1. `remote_path` should be paticularly created and only used to put journal file. Because the tool will clean up the folder every time.
-Suggestion is to create your folder here: `/root/usr/journal/yourUniqueName/`
+Suggestion is to create your own folder here: `/root/usr/journal/**yourUniqueName**/`
 
-2. `context_menu` once `enable`, mean you already add the `JourParser` to your contextmenu, if you move the `JourParser.exe` to another place, you need to run `"Usage Step1 and Step2"` as above again.
-
-#### Release Note:
-##### Version 0.1(2017.6.6):
-1. Support ' '(space) in local journal path
-2. Auto add '/' in at the end of your target path in config file
+2. `context_menu` once `enable`, mean you already add the `JourParser` to your contextmenu, if you move the `JourParser.exe` to another place, you need to run `"Usage Step2 and Step3"` as above again.
